5ab71d04691b7705fcb6fb9e9e612bbc484c1014
Jack <maceuge@icloud.com>
2016-09-25 02:27:34 +0000

    FAQ resuelto

--- js/faq.js
+++ js/faq.js
@@ -1,26 +1,19 @@
 
 var dt = document.querySelectorAll("dt");
 var dd = document.querySelectorAll("dd");
-   
-    dt[0].onclick = function () {
-       dd[0].setAttribute("class", "show");
-    }
-    
-    dt[1].onclick = function () {
-       dd[1].setAttribute("class", "show");
-    }
-    
-    dt[2].onclick = function () {
-       dd[2].setAttribute("class", "show");
-    }
-    
-    dt[3].onclick = function () {
-       dd[3].setAttribute("class", "show");
-    }
-    
-    dt[4].onclick = function () {
-       dd[4].setAttribute("class", "show");
-    }
 
+for (var i = 0; i < dt.length; i++) {
+    dd[i].style.display = "none";
+    dt[i].addEventListener("click", function(e) { mostrar(e.target.getAttribute("data-target")) });
+}
+
+function mostrar (bot) {
+ 
+    if (dd[bot].style.display == "none") {
+        dd[bot].style.display = "block";
+    } else {
+      dd[bot].style.display = "none";
+    }  
+}
  
  
