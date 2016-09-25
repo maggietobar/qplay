5ff6e40a497b3b9f264946f480312b601478181c
Jack <maceuge@icloud.com>
2016-09-25 16:33:24 +0000

    ajustes en login validate

--- js/logval.js
+++ js/logval.js
@@ -7,64 +7,71 @@ var errinputs = document.getElementsByClassName("form-group");
 var error = document.getElementsByClassName("errcript");
 
 
-var validarFormulario = {
+var validarForm = {
     
     vacio: "",
     minlargo: 8,
     mailreg: /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i,
     passreg: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
     
-    validar: function () {
-        
-    if (mail.value != this.vacio) { 
-        if (this.mailreg.test(mail.value)) {
-            errinputs[0].setAttribute("class","form-group");
-            error[0].innerHTML = "";
-            if (pass.value != this.vacio) {
-                if (pass.value.length >= this.minlargo) {
-                    if (this.passreg.test(pass.value)) {
-                        if (pass.value != mail.value) {
-                            errinputs[1].setAttribute("class","form-group");
-                            error[1].innerHTML = "";
-                            form.submit();
-                        } else {
-                            errinputs[1].setAttribute("class","form-group onerror");
-                            form.pass.focus();
-                            error[1].innerHTML = "<b>ERROR!</b> La contraseña no puede ser igual al mail.";
-                        } // fin 1 if else
-                    } else { 
-                        errinputs[1].setAttribute("class","form-group onerror");
-                        form.pass.focus();
-                        error[1].innerHTML = "<b>ERROR!</b> La contraseña tiene que tener al menos una letra chica, una grande y un numero.";
-                    }  // fin 2 if else
-                } else { 
-                    errinputs[1].setAttribute("class","form-group onerror");
-                    form.pass.focus();
-                    error[1].innerHTML = "<b>ERROR!</b> La Contraseña tiene que tener minimo 8 caracteres.";
-                }  // fin 3 if else  
+    validarMail: function () {
+        if (mail.value != this.vacio) { 
+            if (this.mailreg.test(mail.value)) {
+                errinputs[0].setAttribute("class","form-group");
+                error[0].innerHTML = "";
+                return true;
             } else {
-                errinputs[1].setAttribute("class","form-group onerror");
-                form.pass.focus();
-                error[1].innerHTML = "<b>ERROR!</b> El campo contraseña no puede estar vacio.";
-            }  // fin 4 if else
+                errinputs[0].setAttribute("class","form-group onerror");
+                form.mail.focus();
+                error[0].innerHTML = "<b>ERROR!</b> El Email ingresado no es valido.";
+            }  // fin 5 if else  
         } else {
             errinputs[0].setAttribute("class","form-group onerror");
             form.mail.focus();
-            error[0].innerHTML = "<b>ERROR!</b> El Email ingresado no es valido.";
-        }  // fin 5 if else  
-    } else {
-        errinputs[0].setAttribute("class","form-group onerror");
-        form.mail.focus();
-        error[0].innerHTML = "<b>ERROR!</b> El campo email no puede estar vacio.";
-    } // fin 6 if else
-        
-    } // fin validar()
+            error[0].innerHTML = "<b>ERROR!</b> El campo email no puede estar vacio.";
+        } // fin 6 if else        
+    }, // fin validarMail() 
+            
+    validarPass: function () {       
+        if (pass.value != this.vacio) {
+            if (pass.value.length >= this.minlargo) {
+                if (this.passreg.test(pass.value)) {
+                    if (pass.value != mail.value) {
+                        errinputs[1].setAttribute("class","form-group");
+                        error[1].innerHTML = "";
+                        return true;
+                    } else {
+                        errinputs[1].setAttribute("class","form-group onerror");
+                        form.pass.focus();
+                        error[1].innerHTML = "<b>ERROR!</b> La contraseña no puede ser igual al mail.";
+                    } // fin 1 if else
+                } else { 
+                    errinputs[1].setAttribute("class","form-group onerror");
+                    form.pass.focus();
+                    error[1].innerHTML = "<b>ERROR!</b> La contraseña tiene que tener al menos una letra chica, una grande y un numero.";
+                }  // fin 2 if else
+            } else { 
+                errinputs[1].setAttribute("class","form-group onerror");
+                form.pass.focus();
+                error[1].innerHTML = "<b>ERROR!</b> La Contraseña tiene que tener minimo 8 caracteres.";
+            }  // fin 3 if else  
+        } else {
+            errinputs[1].setAttribute("class","form-group onerror");
+            form.pass.focus();
+            error[1].innerHTML = "<b>ERROR!</b> El campo contraseña no puede estar vacio.";
+        }  // fin 4 if else
+    } // fin validarPass()
     
 } // fin validarFormulario
     
     form.onsubmit = function(evt) {
         evt.preventDefault();
-        return validarFormulario.validar();
+        if (validarForm.validarMail() && validarForm.validarPass()) {
+             form.submit();
+        } else {
+            validarForm.validarMail();
+            validarForm.validarPass();
+        }
     }
     
 }  // fin window onload   
